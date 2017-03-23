cca37f3cce31c9a61f16798f84d692da9e0314fe
George Markham <markhamg7@gmail.com>
2017-03-23 15:39:20 +0000

    Added schema.js

---
+++ schema.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var Schema = new mongoose.Schema;
+
+var workoutSchema = new Schema({
+    dumbbell_id: String,
+    user_id: String,
+    date: Date,
+    workout: String,
+    reps: Number,
+    form: Number
+});
+
+var userSchema = new Schema({
+    workouts: [String],
+    date_Joined: Date,
+    email: String,
+    password: String,
+    current_dumbell_id: String, 
+});
