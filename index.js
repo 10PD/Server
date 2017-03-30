77f7a87c968ec0bba794c0e4206f1609c6c51a41
George Markham <georgemarkham@Georges-MacBook-Air.local>
2017-03-30 11:56:11 +0100

    added body-parser and now recieves JSON data

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var router = express.Router();
-mongoose.connect('mongodb://localhost/smartbell');
+//mongoose.connect('mongodb://localhost/smartbell');
 
-var workoutModel = mongoose.model('Workout', schema.workout);
-//var user = new models.User();
+var User = require('./models/user.js');
+var Workout = require('./models/workout');
+
+app.use(bodyParser.json());
 
 router.get('/', function(req,res){
     res.send("Hi there!");
@@ -16,6 +19,27 @@ router.get('/linkDumbbell/:id', function(req,res){
 });
 router.post('/workoutData', function(req,res){
     //store workout data in database
+    var dumbellId = req.body.dumbellId;
+    //***search for user linked to dumbell id***
+    var userId = "184278743";
+    var date = new Date();
+    var workout = req.body.workout;
+    var reps = req.body.reps;
+    var form = req.body.form;
+    res.json({
+        dumbbell_id: dumbellId,
+        user_id: "12345678",
+        date: date,
+        workout: workout,
+        reps: reps,
+        form: form
+    });
+    console.log(dumbellId);
+    //res.json(req.body.dumbellId);
+
+    var newWorkout = new Workout({
+
+    })
 })
 router.get('/workoutData/:id', function(req,res){
     //return workout data for a specific user
